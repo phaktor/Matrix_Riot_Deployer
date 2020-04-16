f2de734a0b375b9fa5008febdd44580ef53943be
Cascaida <onur.pulasli@sony.com>
2020-04-16 20:25:58 +0300

    watch for the warnings

--- main.tf
+++ main.tf
@@ -6,6 +6,10 @@ provider "aws" {
 }
 
 #https://learn.hashicorp.com/terraform/getting-started/install.html - to install terraform
+#type 'terraform init' before you start.
+#aws_access_key_id = "type your IAM access key id to here"
+#aws_secret_access_key = "type your IAM secret access key to here"
+#Put above two lines to secret-variables.auto.tfvars file.
 
 module "VPC" {
   source = "./VPC"
