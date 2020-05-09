8a5ea26bd17334f46d85c2ef1e26a98c1e539e43
phaktor <panxer3@gmail.com>
2020-05-09 18:41:00 +0000

    Added Jitsi container

--- Scripts/matrix_installer.sh
+++ Scripts/matrix_installer.sh
@@ -14,3 +14,14 @@ docker run -d --name synapse \
     --mount type=volume,src=synapse-data,dst=/data \
     -p 8008:8008 \
     matrixdotorg/synapse:latest
+
+yum -y update
+yum install git
+git clone https://github.com/jitsi/docker-jitsi-meet && cd docker-jitsi-meet
+cp env.example .env
+./gen-passwords.sh
+mkdir -p ~/.jitsi-meet-cfg/{web/letsencrypt,transcripts,prosody/config,prosody/prosody-plugins-custom,jicofo,jvb,jigasi,jibri}
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
+sudo chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose up -d
